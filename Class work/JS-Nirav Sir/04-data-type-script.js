b92eb0d7b31d71af4c46c6aaa60ee491abc04e9f
Dhruv6234 <prajapatidhruv6234@gmail.com>
2023-01-02 12:38:59 +0530

    ok

---
+++ Class work/JS-Nirav Sir/04-data-type-script.js
@@ -0,0 +1,22 @@
+document.write('this is external js ')
+
+var x= -12;
+
+console.log(x);
+console.log(typeof(x));   //number
+
+
+var switch1= '1231265  5465 a b c 72 &';
+console.log(switch1);
+console.log(typeof(switch1));
+
+
+var _abs= true;
+console.log(_abs);
+console.log(typeof(_abs));
+
+var xyz;
+console.log(xyz)
+console.log(typeof(xyz))
+
+
